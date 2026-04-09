4a702b677701bd18cd88c0b7988c24039fedca50
Egor Demidov <ed242@njit.edu>
2026-04-09 12:00:46 -0700

    Changed the uptake coefficient plots. Backup before adding pressure-dependent Dg

--- paper_plots/derived_params.py
+++ paper_plots/derived_params.py
@@ -1,3 +1,4 @@
+from scipy.signal import argrelextrema
 import json
 import string
 import matplotlib.pyplot as plt
@@ -41,6 +42,12 @@ levoglucosan = {
     "rho": 1.69     # g/cm^3
 }
 
+def find_local_minimum_before_gmax(arr):
+    imax = np.argmax(arr)
+    mins = argrelextrema(arr[:imax], np.less)[0]
+    idx = mins[-1] if len(mins) > 0 else None
+    return idx
+
 compounds = [nacl, levoglucosan]
 
 fig, axes = plt.subplots(1, 2, figsize=(10, 4.3))
@@ -61,6 +68,36 @@ for compound, ax in zip(compounds, axes):
     S_tot = ufloat(data["solution"]["S_tot"], data["standard_error"]["S_tot"])
     Y_tot = ufloat(data["solution"]["Y_tot"], data["standard_error"]["Y_tot"])
 
+    # Calculating uptake coefficients the old way
+    X_conc = data["fitted_data"]["X"]
+    L = input_data["L"]
+    F_stp = input_data["F"]
+    pressure = input_data["pressure"]
+    Dg = input_data["Di"]
+    A = np.pi * R**2.0
+    F = F_stp * 760.0 / pressure
+    u = F / A
+
+    dX_conc = np.diff(X_conc)
+    idx_ss = find_local_minimum_before_gmax(dX_conc)
+    # plt.plot(range(len(dX_conc)), dX_conc)
+    # plt.plot([idx_ss], [dX_conc[idx_ss]], 'or')
+    # plt.show()
+    # exit()
+
+    I_B = np.min(X_conc)
+    I_A = X_conc[0]
+    I_C = X_conc[idx_ss - 1]
+
+    def gamma_classic(I_0, I_t):
+        k_obs = u / L * np.log(I_0 / I_t)
+        k_diff = 3.66 * Dg / R**2.0
+        k_r_eff = 1.0 / (1.0 / k_obs - 1.0 / k_diff)
+        return k_r_eff * R / 2.0 / omega
+
+    gamma_0_classic = gamma_classic(I_A, I_B)
+    gamma_qss_classic = gamma_classic(I_A, I_C)
+
     ts = np.array(input_data["experimental_data"]["t_exp"])
     t_ads_start = np.array(input_data["t_ads_start"])
     t_ads_end = np.array(input_data["t_ads_end"])
@@ -75,7 +112,7 @@ for compound, ax in zip(compounds, axes):
     gamma_qss_nominal = unp.nominal_values(gamma_qss)
     dXs_dt = r_uptake - k_rxn.n * Xs * (Y_tot.n - P)
 
-    qss_idx = np.abs(ts - compound["gamma_qss_t"]).argmin()
+    # qss_idx = np.abs(ts - compound["gamma_qss_t"]).argmin()
 
     print(f'{compound["name"]} FITTED PARAMETERS')
 
@@ -109,21 +146,29 @@ for compound, ax in zip(compounds, axes):
     print(f"K_sa_tot_inf\t{K_sa_tot_inf:.2uE}")
     print(f"K_p_tot_inf\t{K_p_tot_inf:.2uE}")
     print(f"gamma_0\t\t{gamma_0:.2uE}")
-    print(f"gamma_qss\t{gamma_qss[qss_idx]:.2uE}")
+    print(f"gamma_qss\t{gamma_qss[idx_ss - 1]:.2uE}")
     print(f"C_ml\t\t{compound["C_ml"]:.2e}")
     print(f"chi_S\t\t{chi_S:.2uE}")
     print(f"chi_Y\t\t{chi_Y:.2uE}")
     print(f"ratio_of_chis\t{ratio_of_chis:.2uE}")
+
+    print(f'{compound["name"]} CLASSIC PARAMETERS')
+
+    print(f"gamma_0_classic\t{gamma_0_classic:.2e}")
+    print(f"gamma_qss_classic\t{gamma_qss_classic:.2e}")
 
     # ax2 = ax.twinx()
     ax.plot(ts, gamma, '-k')
     ax.plot(ts, gamma_qss_nominal, '--', color='tab:blue')
     ax.axhline(y=gamma_0.n, linestyle=':', color='tab:red', linewidth=2)
     ax.text(50.0, gamma_0.n - 0.001, f"$\\gamma_0={gamma_0.n:.03f}$", color="tab:red", va="top", ha="left")
-    ax.plot(ts[qss_idx], gamma_qss_nominal[qss_idx], 'o', color='tab:blue')
-    exponent = math.floor(math.log10(abs(gamma_qss_nominal[qss_idx])))
-    mantissa = gamma_qss_nominal[qss_idx] / 10**exponent
-    ax.text(ts[qss_idx]+10, gamma_qss_nominal[qss_idx] + 0.001, "$\\gamma_{\\rm qss}=" + f"{mantissa:.01f}\\times 10^" + "{" + f"{exponent}" + "}$", color="tab:blue", va="bottom", ha="left")
+    ax.axhline(y=gamma_0_classic, linestyle=':', color='tab:purple', linewidth=2)
+    ax.text(50.0, gamma_0_classic - 0.001, f"$\\gamma_0={gamma_0_classic:.03f}$", color="tab:purple", va="top", ha="left")
+    ax.axhline(y=gamma_qss_classic, linestyle='--', color='tab:cyan', linewidth=2)
+    ax.plot(ts[idx_ss - 1], gamma_qss_nominal[idx_ss - 1], 'o', color='tab:blue')
+    exponent = math.floor(math.log10(abs(gamma_qss_nominal[idx_ss - 1])))
+    mantissa = gamma_qss_nominal[idx_ss - 1] / 10**exponent
+    ax.text(ts[idx_ss - 1]+10, gamma_qss_nominal[idx_ss - 1] + 0.001, "$\\gamma_{\\rm qss}=" + f"{mantissa:.01f}\\times 10^" + "{" + f"{exponent}" + "}$", color="tab:blue", va="bottom", ha="left")
     # ax2.plot(range(len(dXs_dt)), dXs_dt)
     # ax.set_title(compound["name"])
 
